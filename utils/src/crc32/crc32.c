0fc519774ebe2f9b901cb6a02b57e8d5ac024c1e
dhylands <dhylands@gmail.com>
2006-11-28 07:33:24 +0000

    Fixed Makefiles to use host OS

--- utils/src/crc32/crc32.c
+++ utils/src/crc32/crc32.c
@@ -6,8 +6,8 @@
 *   it under the terms of the GNU General Public License version 2 as
 *   published by the Free Software Foundation.
 *
-*   Alternatively, this software may be distributed under the terms of BSD
-*   license.
+*   Alternatively, this software may be distributed under the terms of the 
+*   BSD license.
 *
 *   See README and COPYING for more details.
 *
