433139aa72bdc4f10c8d25994b3471e5500ae9aa
dhylands <dhylands@gmail.com>
2007-01-01 18:10:08 +0000

    Added support for building the ATTiny series

--- rules/mkRules-avr.mk
+++ rules/mkRules-avr.mk
@@ -12,7 +12,11 @@ ifneq ($(MK_HEX_TARGET),)
 
 download d : $(call MK_HEX_NAME, $(MK_HEX_TARGET))
 	$(ECHO) "Downloading $< ..."
+ifeq ($(MK_AVR_PROGRAMMER),STK500)
+	$(STK500) -cUSB -d$(MK_AVR_MCU_LONG) -e -if$< -pf
+else
 	$(Q)$(BOOTHOST) $(BOOTHOST_OPTS) -b 38400 $<
+endif
 
 .PHONY: download d
 
