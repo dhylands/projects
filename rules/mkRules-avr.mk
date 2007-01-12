ed5721f60acb0f013d8232d639c4a02dab15ffbb
dhylands <dhylands@gmail.com>
2007-01-12 15:52:25 +0000

    Fixed size reporting for AVR modules

--- rules/mkRules-avr.mk
+++ rules/mkRules-avr.mk
@@ -6,6 +6,8 @@
 #
 ###########################################################################
 
+MK_PRINT_ELF_SIZE = avr-size --mcu=$(MK_AVR_MCU_LONG) $@
+
 include $(MK_RULES_DIR)/mkRules-gcc.mk
 
 ifneq ($(MK_HEX_TARGET),)
