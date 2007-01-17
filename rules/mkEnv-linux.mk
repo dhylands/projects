e5f339f7a0cc44d4701a3f2f85e98300254050de
dhylands <dhylands@gmail.com>
2007-01-17 01:13:16 +0000

    Whoops - Forgot to add the linux support files

---
+++ rules/mkEnv-linux.mk
@@ -0,0 +1,14 @@
+###########################################################################
+#
+# 	mkEnv-cygwin.mk
+#
+# 	This file contains cygwin specific defines for the build environment.
+#
+# 	This file should only define variables and not have any rules in it.
+#
+###########################################################################
+
+
+include $(MK_RULES_DIR)/mkEnv-gcc.mk
+
+MK_ELF_EXT    	=
---
+++ rules/mkRules-linux.mk
@@ -0,0 +1,9 @@
+###########################################################################
+#
+#	mkRules-linux.mk
+#
+#	This file contains rules for building under linux
+#
+###########################################################################
+
+include $(MK_RULES_DIR)/mkRules-gcc.mk
