178535c99412b65392a23f5fc7e25b19c5d5a1ff
dhylands <dhylands@gmail.com>
2007-02-22 00:50:11 +0000

    Fixed a minor bug in the counting of CR terminated lines

--- utils/src/CountLineEndings/CountLineEndings.c
+++ utils/src/CountLineEndings/CountLineEndings.c
@@ -31,11 +31,11 @@ int main( int argc, char **argv )
 	FILE	*src_fs;
 	int		ch;
 	int		foundCr;
-    int     lfCount;
-    int     crCount;
-    int     crLfCount;
-    int     crcrLfCount;
-    int     otherCount;
+	int     lfCount;
+	int     crCount;
+	int     crLfCount;
+	int     crcrLfCount;
+	int     otherCount;
 
 	if ( argc < 2 )
 	{
@@ -43,10 +43,10 @@ int main( int argc, char **argv )
 		exit( 1 );
 	}
 
-    printf( "    LF     CR   CRLF CRCRLF  Other FileName\n" );
-    printf( "------ ------ ------ ------ ------ ---------------------------\n" );
+	printf( "    LF     CR   CRLF CRCRLF  Other FileName\n" );
+	printf( "------ ------ ------ ------ ------ ---------------------------\n" );
 
-    for ( i = 1; i < argc; i++ )
+	for ( i = 1; i < argc; i++ )
 	{
 		if (( src_fs = fopen( argv[ i ], "rb" )) == NULL )
 		{
@@ -54,51 +54,45 @@ int main( int argc, char **argv )
 			exit( 1 );
 		}
 
-        foundCr = 0;
-        lfCount = 0;
-        crCount = 0;
-        crLfCount = 0;
-        crcrLfCount = 0;
-        otherCount = 0;
+		foundCr = 0;
+		lfCount = 0;
+		crCount = 0;
+		crLfCount = 0;
+		crcrLfCount = 0;
+		otherCount = 0;
 
 		while (( ch = fgetc( src_fs )) != EOF )
 		{
-            if ( ch == 0x0D )
-            {
-                foundCr++;
-                continue;
-            }
+			if ( ch == 0x0D )
+			{
+				foundCr++;
+				continue;
+			}
 
-            if ( ch == 0x0A )
-            {
-                switch ( foundCr )
-                {
-                    case 0:     lfCount++;      break;
-                    case 1:     crLfCount++;    break;
-                    case 2:     crcrLfCount++;  break;
-                    default:    otherCount++;   break;
-                }
+			if ( ch == 0x0A )
+			{
+				switch ( foundCr )
+				{
+					case 0:     lfCount++;      break;
+					case 1:     crLfCount++;    break;
+					case 2:     crcrLfCount++;  break;
+					default:    otherCount++;   break;
+				}
 
+				foundCr = 0;
+				continue;
+			}
+
+			if ( foundCr > 0 )
+			{
+                crCount += foundCr;
                 foundCr = 0;
-                continue;
-            }
-
-            if ( foundCr > 0 )
-            {
-                if ( foundCr == 1 )
-                {
-                    crCount++;
-                }
-                else
-                {
-                    otherCount++;
-                }
-            }
+			}
 		}
 
 		fclose(src_fs);
 
-        printf( "%6d %6d %6d %6d %6d %s\n", lfCount, crCount, crLfCount, crcrLfCount, otherCount, argv[ i ] );
+		printf( "%6d %6d %6d %6d %6d %s\n", lfCount, crCount, crLfCount, crcrLfCount, otherCount, argv[ i ] );
 	}
 
 	exit( 0 );
