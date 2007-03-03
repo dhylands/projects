6f83f43552616d91b8160db9888635b1ceba7caf
dhylands <dhylands@gmail.com>
2007-03-03 23:30:53 +0000

    Added ARM compiler (for luminary Cortex-M3) into PATH

--- bash/dot-startup.d/02-path.sh
+++ bash/dot-startup.d/02-path.sh
@@ -8,9 +8,13 @@
 
 PATH='/usr/local/bin'
 PATH=${PATH}:"${HOME}/Utils"
+PATH=${PATH}:'/c/Program Files/ImageMagick-6.2.8-Q16'
 PATH=${PATH}:'/bin'
 PATH=${PATH}:'/usr/bin'
+PATH=${PATH}:'/usr/X11R6/bin'
 PATH=${PATH}:'.'
+PATH=${PATH}:'/c/WinAVR/bin'
+PATH=${PATH}:'/c/Program Files/CodeSourcery/Sourcery G++/bin'
 PATH=${PATH}:'/c/WINDOWS'
 PATH=${PATH}:'/c/WINDOWS/system32'
 PATH=${PATH}:'/c/WINDOWS/system32/Wbem'   # Needed for netsh
