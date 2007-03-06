fe7df6b0388e9b92deb75958e5ecc55d8d2b9fd1
dhylands <dhylands@gmail.com>
2007-03-06 09:07:42 +0000

    Added 'd' and 'e' to toggle LED on bioloid-imu

--- host/bioloid-servo-test/servo-test.c
+++ host/bioloid-servo-test/servo-test.c
@@ -263,11 +263,13 @@ int main( int argc, char **argv )
 
     while ( 1 )
     {
-        static  char    seqEntry[3][9] = 
+        static  char    seqEntry[5][9] = 
         {
-            { 255, 255, 5, 5, 3, 30,  44, 1, 167 },
-            { 255, 255, 5, 5, 3, 30, 255, 1, 212 },
-            { 255, 255, 5, 5, 3, 30, 188, 2,  22 }
+            { 255, 255,   5, 5, 3, 30,  44,   1, 167 },
+            { 255, 255,   5, 5, 3, 30, 255,   1, 212 },
+            { 255, 255,   5, 5, 3, 30, 188,   2,  22 },
+            { 255, 255, 120, 4, 3, 25,   1, 102 },
+            { 255, 255, 120, 4, 3, 25,   0, 103 }
         };
         char *seq;
 
@@ -288,6 +290,17 @@ int main( int argc, char **argv )
             {
                 seq = &seqEntry[ ch - 'a' ][ 0 ];
                 if ( write( gPortFd, seq, 9 ) != 9 )
+                {
+                    fprintf( stderr, "Write to serial port failed: %s\n", strerror( errno ));
+                }
+                break;
+            }
+
+            case 'd':
+            case 'e':
+            {
+                seq = &seqEntry[ ch - 'a' ][ 0 ];
+                if ( write( gPortFd, seq, 8 ) != 8 )
                 {
                     fprintf( stderr, "Write to serial port failed: %s\n", strerror( errno ));
                 }
