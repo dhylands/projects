bebef81046674d1d628ebcdb12cae1f1d4d862eb
jhylands <jon@huv.com>
2007-03-22 23:36:20 +0000

    LCD for ATmega88/168

--- avr/lcd-robostix/Config.h
+++ avr/lcd-robostix/Config.h
@@ -16,24 +16,25 @@
 #if !defined( CONFIG_H )
 #define CONFIG_H
 
-#define CFG_USE_UART0   1
+/*
+#define CFG_USE_UART0   0
 #define CFG_USE_UART1   0
 
 #define CFG_UART0_BAUD_RATE         38400
 #define CFG_UART0_RX_BUFFER_SIZE    128
 #define CFG_UART0_TX_BUFFER_SIZE    128
 #define CFG_UART0_LF_TO_CRLF        1
-
-#define CFG_LCD_DATA_PORT   PORTA
+*/
+#define CFG_LCD_DATA_PORT   PORTD
 #define CFG_LCD_DATA_SHIFT  0
 
-#define CFG_LCD_E_PORT      PORTA
-#define CFG_LCD_E_PIN       6
+#define CFG_LCD_E_PORT      PORTD
+#define CFG_LCD_E_PIN       4
 
-#define CFG_LCD_RS_PORT     PORTA
-#define CFG_LCD_RS_PIN      4
+#define CFG_LCD_RS_PORT     PORTD
+#define CFG_LCD_RS_PIN      6
                 
-#define CFG_LCD_RW_PORT     PORTA
+#define CFG_LCD_RW_PORT     PORTD
 #define CFG_LCD_RW_PIN      5
 
 #endif  // CONFIG_H
