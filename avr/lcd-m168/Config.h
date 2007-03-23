2a2c56f84dbb1503c286b4461bcd51d61471ac76
dhylands <dhylands@gmail.com>
2007-03-23 07:32:52 +0000

    Added sample which drives LCD from mega 168

---
+++ avr/lcd-m168/Config.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+*
+*   Copyright (c) 2006 Dave Hylands     <dhylands@gmail.com>
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License version 2 as
+*   published by the Free Software Foundation.
+*
+*   Alternatively, this software may be distributed under the terms of BSD
+*   license.
+*
+*   See README and COPYING for more details.
+*
+****************************************************************************/
+
+#if !defined( CONFIG_H )
+#define CONFIG_H
+
+#define CFG_USE_UART0   0
+#define CFG_USE_UART1   0
+
+#define CFG_UART0_BAUD_RATE         38400
+#define CFG_UART0_RX_BUFFER_SIZE    128
+#define CFG_UART0_TX_BUFFER_SIZE    128
+#define CFG_UART0_LF_TO_CRLF        1
+
+#define CFG_LCD_DATA_PORT   PORTD
+#define CFG_LCD_DATA_SHIFT  0
+
+#define CFG_LCD_E_PORT      PORTD
+#define CFG_LCD_E_PIN       4
+
+#define CFG_LCD_RS_PORT     PORTD
+#define CFG_LCD_RS_PIN      6
+                
+#define CFG_LCD_RW_PORT     PORTD
+#define CFG_LCD_RW_PIN      5
+
+#endif  // CONFIG_H
+
+
---
+++ avr/lcd-m168/lcd-m168.c
@@ -0,0 +1,50 @@
+/****************************************************************************
+*
+*   Copyright (c) 2006 Dave Hylands     <dhylands@gmail.com>
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License version 2 as
+*   published by the Free Software Foundation.
+*
+*   Alternatively, this software may be distributed under the terms of BSD
+*   license.
+*
+*   See README and COPYING for more details.
+*
+****************************************************************************/
+
+#include <avr/io.h>
+
+#include "lcd.h"
+#include "Delay.h"
+#include "Timer.h"
+
+#define LED_DDR        DDRB
+#define LED_PORT       PORTB
+#define LED_MASK       ( 1 << 1  )
+
+int main(void)
+{
+    InitTimer();
+
+    LED_DDR |= LED_MASK;
+
+    LCD_Init( 2, 8 );
+
+    LCD_PutStr( "**** R/C Input ****\n" );
+    LCD_PutStr( "LCD Test Line 6 1234\n" );
+
+    while( 1 )
+    {
+        LED_PORT &= ~LED_MASK;
+
+        ms_spin( 100 );
+
+        LED_PORT |=  LED_MASK;
+
+        ms_spin( 100 );
+    }
+
+    return 0;
+}
+
