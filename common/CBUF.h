b29b70acbbf73f1e56aba7dfb3246d81af231cec
dhylands <dhylands@gmail.com>
2007-05-22 07:07:04 +0000

    Added some doxygen comments

--- common/CBUF.h
+++ common/CBUF.h
@@ -13,8 +13,7 @@
 *   @defgroup   CBUF Circular Buffer
 *   @{
 *
-*   @brief  This file contains global definitions for circular buffer
-*           manipulation.
+*   @brief  A simple and efficient set of circular buffer manipulations.
 *
 *   These macros implement a circular buffer which employs get and put
 *   pointers, in such a way that mutual exclusion is not required
@@ -32,22 +31,23 @@
 *   you need to define a macro for the size.
 *
 *   First, you need to name your circular buffer. For this example, we'll
-*   call it myQ.
+*   call it @c myQ.
 *
 *   The size macro that needs to be defined will be the name of the
-*   circular buffer followed by _SIZE. The size must be a power of two
+*   circular buffer followed by @c _SIZE. The size must be a power of two
 *   and it needs to fit in the get/put indicies. i.e. if you use an
 *   8 bit index, then the maximum supported size would be 128.
 *
 *   The structure which defines the circular buffer needs to have 3 members
-*   m_getIdx, m_putIdx, and m_entry.
+*   <tt>m_getIdx, m_putIdx,</tt> and @c m_entry.
 *
-*   m_getIdx and m_putIdx need to be unsigned integers of the same size.
+*   @c m_getIdx and @c m_putIdx need to be unsigned integers of the same size.
 *
-*   m_entry needs to be an array of xxx_SIZE entries, or a pointer to an
-*   array of xxx_SIZE entries. The type of each entry is entirely up to the
+*   @c m_entry needs to be an array of @c xxx_SIZE entries, or a pointer to an
+*   array of @c xxx_SIZE entries. The type of each entry is entirely up to the
 *   caller.
 *
+*   @code
 *   #define myQ_SIZE    64
 *   
 *   volatile struct
@@ -57,16 +57,19 @@
 *       uint8_t     m_entry[ myQ_SIZE ];
 *
 *   } myQ;
+*   @endcode
 *
-*   You could then use
+*   You could then use CBUF_Push to add a character to the circular buffer:
 *
-*       CBUF_Push( myQ, 'x' );
+*   @code
+*   CBUF_Push( myQ, 'x' );
+*   @endcode
 *
-*   to add a character to the circular buffer, or
+*   And CBUF_Pop to retrieve an element from the buffer:
 *
-*       ch = CBUF_Pop( myQ );
-*
-*   to retrieve an element from the buffer.
+*   @code
+*   ch = CBUF_Pop( myQ );
+*   @endcode
 *
 *   If you happen to prefer to use C++ instead, there is a templatized
 *   version which requires no macros. You just declare 3 template parameters:
@@ -76,8 +79,9 @@
 *       - The type that should be used for the entry
 *
 *   For example:
-*
-*       CBUF< uint8_t, 64, char >   myQ;
+*   @code
+*   CBUF< uint8_t, 64, char >   myQ;
+*   @endcode
 *
 ****************************************************************************/
 
@@ -88,7 +92,7 @@
 
 /* ---- Constants and Types ---------------------------------------------- */
 
-/**
+/** 
 *   Initializes the circular buffer for use.
 */ 
 
@@ -102,28 +106,12 @@
 #define CBUF_Len( cbuf )        ((typeof( cbuf.m_putIdx ))(( cbuf.m_putIdx ) - ( cbuf.m_getIdx )))
 
 /**
-*   Appends an element to the end of the circular buffer
+*   Appends an element to the end of the circular buffer. The
+*   element is expected to be of the same type as the @c m_entry
+*   member.
 */
 
 #define CBUF_Push( cbuf, elem ) (cbuf.m_entry)[ cbuf.m_putIdx++ & (( cbuf##_SIZE ) - 1 )] = (elem)
-
-/**
-*   Returns a pointer to the last spot that was pushed.
-*/
-
-#define CBUF_GetLastEntryPtr( cbuf ) &(cbuf.m_entry)[ ( cbuf.m_putIdx - 1 ) & (( cbuf##_SIZE ) - 1 )]
-
-/**
-*   Returns a pointer to the next spot to push.
-*/
-
-#define CBUF_GetPushEntryPtr( cbuf ) &(cbuf.m_entry)[ cbuf.m_putIdx & (( cbuf##_SIZE ) - 1 )]
-
-/**
-*   Advances the put index.
-*/
-
-#define CBUF_AdvancePushIdx( cbuf )  cbuf.m_putIdx++
 
 /**
 *   Retrieves an element from the beginning of the circular buffer
@@ -132,13 +120,48 @@
 #define CBUF_Pop( cbuf )        (cbuf.m_entry)[ cbuf.m_getIdx++ & (( cbuf##_SIZE ) - 1 )]
 
 /**
-*   Retrieves the i'th element from the beginning of the circular buffer
+*   Returns a pointer to the last spot that was pushed.
+*/
+
+#define CBUF_GetLastEntryPtr( cbuf ) &(cbuf.m_entry)[ ( cbuf.m_putIdx - 1 ) & (( cbuf##_SIZE ) - 1 )]
+
+/**
+*   Returns a pointer to the next spot to push. This can be used
+*   in conjunction with CBUF_AdvancePushIdx to fill out an entry
+*   before indicating that it's available. It is the caller's
+*   responsibility to enure that space is available, and that no
+*   other items are pushed to overwrite the entry returned.
+*/
+
+#define CBUF_GetPushEntryPtr( cbuf ) &(cbuf.m_entry)[ cbuf.m_putIdx & (( cbuf##_SIZE ) - 1 )]
+
+/**
+*   Advances the put index. This is useful if you need to
+*   reserve space for an item but can't fill in the contents
+*   yet. CBUG_GetLastEntryPtr can be used to get a pointer to
+*   the item. It is the caller's responsibility to ensure that
+*   the item isn't popped before the contents are filled in.
+*/
+
+#define CBUF_AdvancePushIdx( cbuf )  cbuf.m_putIdx++
+
+/**
+*   Advances the get index. This is slightly more efficient than
+*   popping and tossing the result.
+*/
+
+#define CBUF_AdvancePopIdx( cbuf )  cbuf.m_getIdx++
+
+/**
+*   Retrieves the <tt>idx</tt>'th element from the beginning of
+*   the circular buffer
 */
 
 #define CBUF_Get( cbuf, idx )        (cbuf.m_entry)[( cbuf.m_getIdx + idx ) & (( cbuf##_SIZE ) - 1 )]
 
 /**
-*   Retrieves the i'th element from the end of the circular buffer
+*   Retrieves the <tt>idx</tt>'th element from the end of the
+*   circular buffer.
 */
 
 #define CBUF_GetEnd( cbuf, idx )        (cbuf.m_entry)[( cbuf.m_putIdx - idx - 1 ) & (( cbuf##_SIZE ) - 1 )]
@@ -150,13 +173,7 @@
 #define CBUF_GetPopEntryPtr( cbuf ) &(cbuf.m_entry)[ cbuf.m_getIdx & (( cbuf##_SIZE ) - 1 )]
 
 /**
-*   Advances the put index.
-*/
-
-#define CBUF_AdvancePopIdx( cbuf )  cbuf.m_getIdx++
-
-/**
-*   Determines if the circular buffer is empty
+*   Determines if the circular buffer is empty.
 */
 
 #define CBUF_IsEmpty( cbuf )    ( CBUF_Len( cbuf ) == 0 )
