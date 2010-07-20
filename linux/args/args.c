139c9f0e645e01882cb295f324c8445dc7f95eb1
dhylands <dhylands@gmail.com>
2010-07-20 06:15:21 +0000

    Test program which prints its arguments to stdout

---
+++ linux/args/args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char **argv )
+{
+    int     i;
+
+    for ( i = 0; i < argc; i++ ) 
+    {
+        printf( "argv[ %d ] = '%s'\n", i, argv[ i ] );
+    }
+
+    return 0;
+}
