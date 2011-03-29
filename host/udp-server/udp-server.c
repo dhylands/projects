ed78cf15d5773e2b6033dac1340158b63d00c244
dhylands <dhylands@gmail.com>
2011-03-29 20:49:36 +0000

    Added UDP client and server samepl programs from http://www.abc.se/~m6695/udp.html

---
+++ host/udp-server/udp-server.c
@@ -0,0 +1,51 @@
+/*
+ * Downloaded from http://www.abc.se/~m6695/udp.html
+ */
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFLEN 512
+#define NPACK 10
+#define PORT 9930
+
+static void diep(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+int main(void)
+{
+    struct sockaddr_in si_me, si_other;
+    int s, i;
+    socklen_t slen=sizeof(si_other);
+    char buf[BUFLEN];
+    
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+        diep("socket");
+    
+    memset((char *) &si_me, 0, sizeof(si_me));
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(PORT);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(s, (struct sockaddr *)&si_me, sizeof(si_me))==-1)
+        diep("bind");
+    
+    for (i=0; i<NPACK; i++) {
+        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&si_other, &slen)==-1)
+            diep("recvfrom()");
+        printf("Received packet from %s:%d\nData: %s\n\n", 
+               inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
+    }
+    
+    close(s);
+    return 0;
+}
+
