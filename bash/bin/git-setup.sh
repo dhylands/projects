6a4d279172bd2dae2706cd3e4dd4760c593ca752
dhylands <dhylands@gmail.com>
2011-09-21 19:05:58 +0000

    Fixed escaping/quoting on git-setup.sh

--- bash/bin/git-setup.sh
+++ bash/bin/git-setup.sh
@@ -2,13 +2,13 @@
 #
 # This script does some common git setup that I do for any new account/machine I use.
 #
+set -x
 
 git config --global user.name "Dave Hylands"
 git config --global user.email "dhylands@broadcom.com"
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
-
-git config --global alias.status-report  \!sh -c "git log --pretty='%C(yellow)%h%Creset %cn %Cgreen%cr%Creset %s\'" \| grep Hylands
-git config --global alias.summary-report \!sh -c "git log --pretty='%C(yellow)%h%Creset %cn %Cgreen%cr%Creset %s\'"
+git config --global alias.status-report  '!sh -c "git log --pretty='\''%C(yellow)%h%Creset %cn %Cgreen%cr%Creset %s'\''" | grep Hylands'
+git config --global alias.summary-report '!sh -c "git log --pretty='\''%C(yellow)%h%Creset %cn %Cgreen%cr%Creset %s'\''"'
 
